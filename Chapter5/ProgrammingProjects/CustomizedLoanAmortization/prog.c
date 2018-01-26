9ffdd75e26346647741889a5176ba005403dcd76
Nikhil <nikhilnayak98@gmail.com>
2018-01-26 16:35:36 +0530

    amortization table

--- Chapter5/ProgrammingProjects/CustomizedLoanAmortization/prog.c
+++ Chapter5/ProgrammingProjects/CustomizedLoanAmortization/prog.c
@@ -5,22 +5,33 @@ Desc - To create an output file containing a customized loan amortization table.
 */
 #include "stdio.h"
 #include "math.h"
-double payment_cal(double, double, int);
 void main()
 {
-	int n;
-	double principal, annual_interest_rate;
+	int n, i;
+	double principal, annual_interest_rate, monthly_interest_rate, monthly_payment, balance, principal_monthly, each_interest;
 	printf("\nEnter amount borrowed (princpal) - ");
 	scanf("%lf", &principal);
 	printf("\nEnter annual interest rate - ");
 	scanf("%lf", &annual_interest_rate);
-	printf("\nEnter number of payments - ");
-	scanf("%lf", &n);
-}
+	printf("\nEnter number of payments(months) - ");
+	scanf("%d", &n);
+	
+	monthly_interest_rate = annual_interest_rate / 1200;
+	monthly_payment = principal * monthly_interest_rate / (1 - 1 / pow(1 + monthly_interest_rate, n));
+	
+	printf("\nTotal Payment - %.2f", monthly_payment * n);
 
-double payment_cal(double principle, double annual_interest, int total_payments)
-{
-	double payment = (((double)annual_interest / 1200) * principle)/(1 - pow((1 + annual_interest / 1200), -1 * total_payments));
-	return payment; 
+	balance = principal;
+		
+	printf("\nPayment %8c Interest %8c Principal %8c Balance", ' ', ' ', ' ');
+	
+	for(i = 0; i < n; i++) 
+	{
+		each_interest = monthly_interest_rate * balance;
+		principal_monthly = monthly_payment - each_interest;
+		balance = balance - principal_monthly;
+		
+		printf("\n %d %13c %.2f %13c %.2f %13c %.2f", (i + 1), ' ', each_interest, ' ', principal_monthly, ' ', balance);
+	}
+	printf("\nFinal Payment - %.2f\n", monthly_payment);
 }
-
