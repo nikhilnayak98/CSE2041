841884717b75f338e7d7bb0ca88c3cf40b96eee7
Nikhil Nayak <nikhilnayak98@gmail.com>
2018-02-01 11:05:27 +0530

    area under curve

--- Chapter5/ProgrammingProjects/AreaUnderCurve/prog.c
+++ Chapter5/ProgrammingProjects/AreaUnderCurve/prog.c
@@ -5,22 +5,29 @@ Desc - To calculate area under curve.
 */
 #include "stdio.h"
 #include "math.h"
-void trap(double, double, int, double);
-double func(double);
+void trap(double, double, int, double, double func(double));
+double func1(double);
+double func2(double);
 void main()
 {
-	double a, b;
 	int n;
 	
+	printf("\nfor sqrt(4 − x^2)");
 	for(n = 2; n <= 128; n *= 2)
 	{
 		printf("\nFor n = %d", n);
-		trap(-2, 2, n, 0);	// for sqrt(4 − x^2)
-		//trap(0, 3.14159, n, 0);	// for x^2sin(x)
+		trap(-2, 2, n, 0, func1);
+	}
+	
+	printf("\n\nfor x^2sin(x)");
+	for(n = 2; n <= 128; n *= 2)
+	{
+		printf("\nFor n = %d", n);
+		trap(0, 3.14159, n, 0, func2);	// for x^2sin(x)
 	}
 }
 
-void trap(double a, double b, int n, double f)
+void trap(double a, double b, int n, double f, double func(double))
 {
 	double h, T = 0, temp = a;
 	int i;
@@ -39,13 +46,13 @@ void trap(double a, double b, int n, double f)
 }
 
 // h(x) = sqrt(4 − x^2)
-double func(double x)
+double func1(double x)
 {
 	return sqrt(4 - (x * x));
 }
 
 // g(x) = x^2 * sin(x)
-double func1(double x)
+double func2(double x)
 {
 	return (x * x * sin(x));
 }
