4b9f4141b2ab2e322f8bdd56742659f9a9ecf3a4
Nikhil Nayak <nikhilnayak98@gmail.com>
2018-03-03 07:44:55 +0000

    Simple Calculator

--- Chapter6/ProgrammingProjects/SimpleCalculator/prog.c
+++ Chapter6/ProgrammingProjects/SimpleCalculator/prog.c
@@ -9,27 +9,26 @@ void do_next_op(char, double, double*);
 void scan_data(char*, double*);
 void main()
 {
-	double accumulator = 0, operand;
-	char operator_symbol;
-	
-	printf("\n+ add");
+    double accumulator = 0, operand;
+    char operator_symbol;
+    
+    printf("\n+ add");
     printf("\n- subtract");
     printf("\n* multiply");
     printf("\n/ divide");
     printf("\n^ power (raise left operand to power of right operand)");
     printf("\nq quit\n");
-	
-	scan_data(&operator_symbol, &operand);
-	
-	while(operator_symbol != 'q')
-	{
-	    do_next_op(operator_symbol, operand, &accumulator);
-	    
-	    printf("result so far is %.1f", accumulator);
-	    scan_data(&operator_symbol, &operand);
-	}
-	
-	printf("\nfinal result is is %.1f\n", accumulator);
+    
+    scan_data(&operator_symbol, &operand);
+    
+    while(operator_symbol != 'q')
+    {
+        do_next_op(operator_symbol, operand, &accumulator);
+        
+        printf("result so far is %.1f", accumulator);
+        scan_data(&operator_symbol, &operand);
+    }
+    printf("\nfinal result is is %.1f\n", accumulator);
 }
 
 void do_next_op(char operator_symbol, double operand, double *accumulator)
@@ -56,6 +55,6 @@ void do_next_op(char operator_symbol, double operand, double *accumulator)
 void scan_data(char *operator_symbol, double *operand)
 {
     printf("\n");
-	scanf(" %c", &*operator_symbol);
-	scanf(" %lf", &*operand);
+    scanf(" %c", &*operator_symbol);
+    scanf(" %lf", &*operand);
 }
