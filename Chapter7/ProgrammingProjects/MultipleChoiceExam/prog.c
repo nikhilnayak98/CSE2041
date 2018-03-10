d02e105bb8c055ed6afda789e4098365002073f5
Nikhil <nikhilnayak98@gmail.com>
2018-03-10 20:06:41 +0530

    multiple choice question fixed

--- Chapter7/ProgrammingProjects/MultipleChoiceExam/prog.c
+++ Chapter7/ProgrammingProjects/MultipleChoiceExam/prog.c
@@ -4,111 +4,84 @@ Regd No - 1641012040
 Desc - Multiple Choice Exam
 */
 #include <stdio.h>
-
-void printToFile(int no_of_students, int no_of_qs, int student_id[], char student_answers[], int student_score[], char correct_answers[])
-{
-	int i;
-	FILE *output = fopen("report.txt", "w");
-	fprintf(output, "%6c Exam Report\n", ' ');
-	
-	fprintf(output, "Questions : ");
-	for(i = 0; i < no_of_qs; i++)
-	{
-		fprintf(output, "%d ", (i + 1));
-	}
-	
-	fprintf(output, "\nAnswers : ");
-	
-	for(i = 0; i < no_of_qs; i++)
-	{
-		fprintf(output, "%c ", correct_answers[i]);
-	}
-	
-	fprintf(output, "\n\nID %5c Score(%%)", ' ');
-	for(i = 0; i < no_of_students; i++)
-	{
-		fprintf(output, "%d %4c %d", student_id[i], ' ', student_score[i]);
-	}
-	
-	fprintf(output, "Question : ");
-	for(i = 0; i < no_of_qs; i++)
-	{
-		fprintf(output, "%d ", (i + 1));
-	}
-	
-	/*fprintf(output, "Missed By : ");
-	for(i = 0; i < no_of_qs; i++)
-	{
-		fprintf(output, "%d ", missed[i]);
-	}*/
-	
-	fclose(output);
-}
-
-void fgetAnswers(int no_of_students, int no_of_qs, int student_id[], char student_answers[], int student_score[], char correct_answers[])
-{
-	FILE *input = fopen("examdat.txt", "r");
-	int i = 0;
-	char space;
-	
-	for(i = 0; i < no_of_students; i++)
-	{
-		student_score[i] = 0;
-	}
-	
-	while((fscanf(input, "%d", &student_id[i])) != EOF)
-	{
-		fscanf(input, " %c", &space);
-		
-		fscanf(input, "%c", &student_answers[i]);
-		while(student_answers[i] != '\n')
-		{
-			if(correct_answers[i] == student_answers[i])
-			{
-				student_score[i]++;
-			}
-			fscanf(input, "%c", &student_answers[i]);
-			i++;
-		}
-		
-	}
-	
-	for(i = 0; i < no_of_students; i++)
-	{
-		student_score[i] = student_score[i] * 20;
-	}
-	
-	for(i = 0; i < no_of_students; i++)
-	{
-		printf("\n%d %4c %c", student_id[i], ' ', student_score[i]);
-	}
-	
-	fclose(input);
-}
-
+void fgetAnswers(char[], int, FILE*);
 void main()
 {
-	int no_of_qs, no_of_students, i = 0;
 	FILE *input = fopen("examdat.txt", "r");
+	int n, i;
+
+	char answers[20], space;
 	
-	no_of_students = 3;
+	fscanf(input, "%d", &n);
+	fscanf(input, "%c", &space);
 	
-	int student_id[no_of_students], student_score[no_of_students], missed[no_of_students];
-	char student_answers[no_of_students], space;
-	
-	fscanf(input, "%d", &no_of_qs);
-	char correct_answers[no_of_qs];
-	
-	i = 0;
-	fscanf(input, "%c %c", &space, &correct_answers[i]);
-	while(correct_answers[i] != '\n')
+	for(i = 0; i < n ; i++)
 	{
-		i++;
-		fscanf(input, "%c", &correct_answers[i]);
+		fscanf(input, "%c", &answers[i]);
 	}
 	
-	fclose(input);
+	fgetAnswers(answers, n, input);
+}
+
+
+void fgetAnswers(char answers[], int n, FILE *input)
+{
+	FILE *output = fopen("report.txt", "w");
 	
-	fgetAnswers(input, no_of_students, no_of_qs, student_id, student_answers, student_score, correct_answers);
-	//printToFile(no_of_students, no_of_qs, student_id, student_answers, student_score, correct_answers);
+	int missed[n], i, id, correct;
+	char choice;
+	
+	for(i = 0; i < n; i++)
+	{
+		missed[i] = 0;
+	}
+	
+	fprintf(output, "%10c Exam Report %2c", ' ', ' ');
+	fprintf(output, "\nQuestion %2c", ' ');
+
+	for(i = 0; i < n ; i++)
+	{
+		fprintf(output, "%d ", i);
+	}
+
+	fprintf(output,"\nAnswer %3c", ' ');
+	
+	for(i = 0; i < n ; i++)
+	{
+		fprintf(output, "%c ", answers[i]);
+	}
+	
+	fprintf(output, "\n\nID %2c Score(%%)\n", ' ');
+	
+	while(fscanf(input, "%d", &id) != EOF)
+	{
+		correct = 0;
+		fprintf(output, "%d %2c", id, ' ');
+		fscanf(input, "%c", &choice);
+		
+		for(i = 0; i < n ; i++)
+		{
+			fscanf(input, "%c", &choice);
+			
+			if(choice == answers[i])
+				correct++;
+			else
+				missed[i]++;
+		}
+		fprintf(output, "%d\n", (correct * 100) / n);
+	}
+
+	fprintf(output,"\nQuestion %2c", ' ');
+	
+	for(i = 0; i < n ; i++)
+	{
+		fprintf(output, "%d ", i);
+	}
+
+	fprintf(output,"\nMissed by %2c", ' ');
+
+	for(i = 0; i < n ; i++)
+	{
+		fprintf(output, "%d ", missed[i]);
+	}
 }
